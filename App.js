c40897f679796646b319a7ad98fe8778e398d632
Kenneth88880 <kenneth232628@gmail.com>
2025-09-23 23:26:06 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function App() {
+  
+  console.log("App executed"); //debug setting remove later
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
