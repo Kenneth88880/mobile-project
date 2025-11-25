2d6d20bd7033435afa87a1e8510b1f79777e9a17
preston-j-wong <preston.j.wong@gmail.com>
2025-11-25 15:21:32 -0500

    added the ability to export every user UID

--- services/UserConfig.js
+++ services/UserConfig.js
@@ -1,6 +1,8 @@
 // UserConfig.js
 // Change this value to switch between different user accounts for testing
 // Examples: "user123", "user456", "user789", etc.
+import {UID} from '../screens/SigninScreen';
 
-export const CURRENT_USER_ID = "u3sC0pQydQSZYcRqp2RybTRwHFl2";
+
+export const CURRENT_USER_ID = UID;
 
