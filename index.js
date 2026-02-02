0b96ca821d5d4ca3ca6a3a5650c83cd80e238ad9
Kenneth88880 <kenneth232628@gmail.com>
2026-02-01 20:19:29 -0500

    new dev mode

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+import { registerRootComponent } from "expo";
 
-import { registerRootComponent } from 'expo';
+import App from "./App";
 
-import App from './App';
+if (!__DEV__) {
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// Also suppress in dev
+console.disableYellowBox = true;
+
 registerRootComponent(App);
