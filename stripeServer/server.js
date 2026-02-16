17dd21a5c67890793274d2581316f37be3666f00
preston-j-wong <preston.j.wong@gmail.com>
2026-02-16 13:12:23 -0500

    added in framework for firebase/stripe combo hosting

--- stripeServer/server.js
+++ stripeServer/server.js
@@ -1,45 +1,41 @@
 require('dotenv').config();
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-// This example sets up an endpoint using the Express framework.
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-app.post('/payment-sheet', async (req, res) => {
-  // Use an existing Customer ID if this is a returning customer.
-  const customer = await stripe.customers.create();
-  const customerSession = await stripe.customerSessions.create({
-    customer: customer.id,
-    components: {
-      mobile_payment_element: {
-        enabled: true,
-        features: {
-          payment_method_save: 'enabled',
-          payment_method_redisplay: 'enabled',
-          payment_method_remove: 'enabled'
-        }
+app.post("/payment-sheet", async (req, res) => {
+  try {
+    const customer = await stripe.customers.create();
+    const customerSession = await stripe.customerSessions.create({
+      customer: customer.id,
+      components: {
+        mobile_payment_element: {
+          enabled: true,
+          features: {
+            payment_method_save: "enabled",
+            payment_method_redisplay: "enabled",
+            payment_method_remove: "enabled",
+          },
+        },
       },
-    },
-  });
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 999,
-    currency: 'cad',
-    customer: customer.id,
-    // In the latest version of the API, specifying the `automatic_payment_methods` parameter
-    // is optional because Stripe enables its functionality by default.
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+    });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 999,
+      currency: "cad",
+      customer: customer.id,
+      automatic_payment_methods: { enabled: true },
+    });
 
-  res.json({
-    paymentIntent: paymentIntent.client_secret,
-    customerSessionClientSecret: customerSession.client_secret,
-    customer: customer.id,
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
-  });
-});
-app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
-  console.log("Server is running on port " + (process.env.PORT || 3000));
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      customerSessionClientSecret: customerSession.client_secret,
+      customer: customer.id,
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
 });
