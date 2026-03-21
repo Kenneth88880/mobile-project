ce8a329e964320c46e37e303851f0ca5ec96ef00
preston-j-wong <preston.j.wong@gmail.com>
2026-03-21 14:31:17 -0400

    .

--- app.config.js
+++ app.config.js
@@ -53,7 +53,7 @@ export default {
       ],
     },
     plugins: [
-      "@react-native-firebase/app".
+      "@react-native-firebase/app", 
       "./expo-firebase-plugin.cjs",
       [
         "@stripe/stripe-react-native",
