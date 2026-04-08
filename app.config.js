ee9521a4405a483668e803f66f0dfcd4279ab577
Kenneth88880 <kenneth232628@gmail.com>
2026-04-07 20:23:39 -0400

    just changed version number in app.config

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     name: "Doubly",
     slug: "doubly-yrvn0tmogrdrliugnun4w",
     owner: "doubly-connections-inc",
-    version: "1.0.6",
+    version: "1.0.7",
     orientation: "portrait",
     icon: "./assets/doubly-icon.png",
     userInterfaceStyle: "light",
