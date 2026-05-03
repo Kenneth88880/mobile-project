0e9a99c9cccbc53f871e32ae5e74298e2cb2bd8c
preston-j-wong <preston.j.wong@gmail.com>
2026-05-03 01:20:08 -0400

    chore: implement PolyForm Strict License 1.0.0 for Doubly Connections Inc.

--- app.config.js
+++ app.config.js
@@ -1,3 +1,7 @@
+/**
+ * Required Notice: Copyright 2026 Doubly Connections Inc. (https://github.com/Kenneth88880/mobile-project)
+ * Licensed under the PolyForm Noncommercial License 1.0.0
+ */
 import "dotenv/config";
 
 export default {
