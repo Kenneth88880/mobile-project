9283a6d6f9eec174b1f614a2a50c6cfe8cd9be3d
Kenneth88880 <kenneth232628@gmail.com>
2026-05-05 22:01:21 -0400

    version 12 updated

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     name: "Doubly",
     slug: "doubly-yrvn0tmogrdrliugnun4w",
     owner: "doubly-connections-inc",
-    version: "1.0.11",
+    version: "1.0.12",
     orientation: "portrait",
     icon: "./assets/doubly-icon.png",
     userInterfaceStyle: "light",
